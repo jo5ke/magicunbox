713350950240198e8fa6c04122ce95761a03f5c4
petarchord <petarchord@hotmail.com>
2019-05-17 18:31:16 +0200

    timer done

--- public/js/modalLogic.js
+++ public/js/modalLogic.js
@@ -1,6 +1,4 @@
-/*const models = require("../../models");
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op; */
+
 
 let login = document.getElementById("login")
 let loginModal = M.Modal.init(login);
@@ -130,32 +128,7 @@ let saleBoxes = returnSaleBoxes(); */
 
 
 
-var counter=11000;
-var cnt = document.getElementsByClassName("counter");
-var intervalID = setInterval(convertSec,1000);
-function convertSec()
-{
-    console.log('counter'+counter);
-    if(counter)
-    {
-        let hour = Math.floor(counter/3600);
-        let remainder = counter - hour*3600;
-        let min = Math.floor(remainder/60);
-        let remainderMin = remainder - min*60;
-        let sec = remainderMin % 60;
-        for(var i=0;i<cnt.length;i++)
-        {
-        cnt[i].innerHTML=hour+":"+min+":"+sec;
-        }
-        counter--;
 
-    }
-    else
-    {
-        clearInterval(intervalID);
-    }
-    
-}
 
  
 
