11e5ad245b9f7e5388137565eba3141687304b00
joske <joskekostic@gmail.com>
2019-05-20 10:10:55 +0200

    fields added to migrations, small changes for toaster for views

--- migrations/20181125124813-create-user.js
+++ migrations/20181125124813-create-user.js
@@ -99,7 +99,7 @@ module.exports = {
         defaultValue: 0,
         allowNull: false
       },
-      promoter:{
+      promoter: {
         type: Sequelize.INTEGER,
         defaultValue: 0
       },
@@ -120,9 +120,17 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true
       },
-      passwordResetCode:{
-          type: Sequelize.STRING,
-          allowNull: true
+      passwordResetCode: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      emailToken: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      verified: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
       },
       createdAt: {
         allowNull: true,
